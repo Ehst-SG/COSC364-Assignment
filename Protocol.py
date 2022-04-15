be823d7417e91b36bef9914297b3c720ddd200f8
Seth <sethgunn18@gmail.com>
2022-04-15 15:13:55 +0000

    Setting shit up

---
+++ Protocol.py
@@ -0,0 +1,27 @@
+MAX_GLOBAL_PATH = 15
+
+
+
+
+
+
+class router:
+    __init__(self, id, input, output):
+        self.id = id
+        self.input = input
+        self.output = output
+        self.routingTable = []
+
+
+
+def loadConfig(configFileName):
+
+
+
+
+def parseConfig(file):
+
+
+
+def updateRoutingTable(newEntry):
+    
