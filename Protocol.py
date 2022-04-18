e27fe69e401a434f8e3d02dddcd531a3d74cef81
Jamie Barnes <jamiebarnes964@gmail.com>
2022-04-18 22:47:35 +1200

    Modified to work with only one router

--- Protocol.py
+++ Protocol.py
@@ -1,20 +1,10 @@
 import sys
 
 MAX_GLOBAL_PATH = 15
-ROUTER_LIST = []
 
-
-class Router:
-    def __init__(self, id):
-        self.id = id
-        self.input_ports = []
-        self.output_ports = []
-    def addInputPort(self, port):
-        self.input_ports.append( port )
-    def addOutputPort(self, port, cost):
-        self.output_ports.append( (port, cost) )
-
-
+routerId = None
+inputPorts = []
+outputPorts = {}
 
 
 def loadConfig(configFileName):
@@ -22,80 +12,82 @@ def loadConfig(configFileName):
     Process the config file
     """
 
+    global routerId
+    global inputPorts
+    global outputPorts
+
     cfg = open(configFileName, "r")
     lines = cfg.readlines()
-    count = 0
-
 
     for line in lines:
-        line = line.split(" ") #input-ports 6110, 6201, 7345
+        line = line.split(" ")  # input-ports 6110, 6201, 7345
 
         # Checks the router ID
         if line[0] == "router-id":
             try:
                 id = int(line[1])
-            except:
+            except Exception:
                 raise Exception("Given router ID is not a number.")
 
-            if lid > 0 and id <= 64000:
-                if routerIdExists(id):
-                    newRouter = Router(id)
-                    ROUTER_LIST.append(newRouter)
-                else:
-                    raise Exception(f"Router already with ID {id} already exists")
+            if id > 0 and id <= 64000:
+                routerId = id
             else:
                 raise Exception("Invalid router ID")
 
-
         # Adds the input ports to the router if it exists.
-        else if line[0] == 'input-ports':
+        elif line[0] == 'input-ports':
             ports = []
             for i in range(1, len(line)):
                 ports.append(int(line[i].strip(",")))
             for port in ports:
-                addInputPort(port)
+                inputPorts.append(port)
 
-        else if line[0] == 'output-ports':
+        # Adds input ports
+        elif line[0] == 'output-ports':
             ports = []
             for i in range(1, len(line)):
                 ports.append(int(line[i].strip(",")))
             for port in ports:
                 port = port.split('-')
+                if len(port) != 3:
+                    raise Exception(
+                        "Incorrect output port format given. Expected: output-ports port-cost-id")
+                outputPorts.update({int(port[0]), int(port[1])})
 
 
-def parseConfig(file):
-    pass
-
-
-def routerIdExists(id):
+def checkConfig():
     """
-    Returns true if
+    Checks if required info was provided and was correctly parsed
     """
-    for router in ROUTER_LIST:
-        if router.id == id:
-            return true
-    return false
+
+    global routerId
+    global inputPorts
+    global outputPorts
+
+    if routerId is None:
+        raise Exception("Router ID not given")
+    if len(inputPorts) == 0:
+        raise Exception("No input ports given")
+    if len(outputPorts) == 0:
+        raise Exception("No output ports given")
+
 
 def updateRoutingTable(newEntry):
     pass
 
 
-
-
-
-
 def main(args):
     if len(args) != 1:
-        print(f`Invalid number of args. Expected 1, got {len(args)}\nExiting...`)
+        print(
+            f"Invalid number of args. Expected 1, got {len(args)}\nExiting...")
         exit()
 
     try:
         loadConfig(args[0])
+        checkConfig()
     except Exception as e:
         print(e)
         print("\nExiting...")
-
-
 
 
 if __name__ == "__main__":
